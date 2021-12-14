ed440867a821b9a4c857d8e03b42a4cb005d1e0b
kazuyaishida0916 <94587462+kazuyaishida0916@users.noreply.github.com>
2021-12-14 15:51:55 +0900

    ログインとログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -8,9 +7,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birth_day])
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          validates :nickname, presence: true
          validates :last_name, presence: true
          validates :first_name, presence: true
-         validates :last_nname_kana, presence: true
+         validates :last_name_kana, presence: true
          validates :first_name_kana, presence: true
          validates :birth_day, presence: true
 end
