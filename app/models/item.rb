45feccbacc3b4aa96f4ec56bd4800071562b152a
kazuyaishida0916 <94587462+kazuyaishida0916@users.noreply.github.com>
2021-12-19 16:20:03 +0900

    商品出品機能テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_cost
   belongs_to :shipping_days
+  belongs_to :user
   has_one_attached :image
-   #空の投稿を保存できないようにする
-   validates :image, presence: true
-   validates :item_price, presence: true numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-   validates :item_name, presence: true, length: { maximum: 40 }
-   validates :item_detail, presence: true, length: { maximum: 10000 }
 
-   #ジャンルの選択が「--」の時は保存できないようにする
+   validates :image, presence: true
+   validates :item_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+   validates :item_name, presence: true
+   validates :item_detail, presence: true
+
+  
    validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"} 
    validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
    validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
