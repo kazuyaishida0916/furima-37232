3c53f7ce4308e8603944ea4260b7f9829bc11f30
kazuyaishida0916 <94587462+kazuyaishida0916@users.noreply.github.com>
2021-12-29 15:02:36 +0900

    購入機能修正２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,16 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
   before_action :move_to_index, only: :index
+  before_action :set_item, only: [:index, :create] 
 
   def index
-    @item = Item.find(params[:item_id])
     @card_address = CardAddress.new
-   
   end
 
   def create
     @card_address = CardAddress.new(card_params)
-    @item = Item.find(params[:item_id])
     if @card_address.valid?
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-      Payjp::Charge.create(
-        amount: @item.item_price,
-        card: card_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @card_address.save
       redirect_to root_path
     else
@@ -32,6 +25,20 @@ class OrdersController < ApplicationController
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
   end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+      Payjp::Charge.create(
+        amount: @item.item_price,
+        card: card_params[:token],
+        currency: 'jpy'
+      )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 
   def move_to_index
     @item = Item.find(params[:item_id])
